5477c88d86feab9ebfe87311aeae601920e3e044
Jeremie Gillet <jeremie.gillet@ithera-medical.com>
2024-10-21 09:38:07 +0200

    Trying to fix bug in manual test

---
+++ mcx/utils/islicer.m
@@ -0,0 +1,67 @@
+function h = islicer(vol, T, handles, keepxyz, varargin)
+%
+%    h = islicer(vol);
+%       or
+%    h = islicer(vol,T);
+%
+%    author: Anders Brun, anders@cb.uu.se (2009)
+%    url: https://www.mathworks.com/matlabcentral/fileexchange/25923-myslicer-make-mouse-interactive-slices-of-a-3-d-volume
+%    original version: v1.1 (accessed on 07/25/2017)
+%
+%    input:
+%        vol:  the 3-D or 4-D data being loaded
+%        T: a 4x4 transformation matrix; if missing, T is a 4x4 identity
+%           matrix
+%
+%    output:
+%        h: the handles to the 3 surfaces plotted along the x/y/z
+%           planes
+%
+%    this file is part of Monte Carlo eXtreme (MCX)
+%    License: GPLv3, see http://mcx.sf.net for details
+%
+
+if nargin < 2
+    T = eye(4);
+end
+
+if (nargin < 3)
+    handles = [0, 0, 0];
+end
+
+if (nargin < 4)
+    keepxyz = 0;
+end
+
+if (handles(1) == 0)
+    h1 = slice3i(vol, T, 1, round(size(vol, 1) / 2), handles(1), keepxyz);
+else
+    h1 = slice3i(vol, T, 1, round(min(min(get(handles(1), 'xdata')))), handles(1), keepxyz);
+end
+
+if (handles(2) == 0)
+    h2 = slice3i(vol, T, 2, round(size(vol, 2) / 2), handles(2), keepxyz);
+else
+    h2 = slice3i(vol, T, 2, round(min(min(get(handles(2), 'ydata')))), handles(2), keepxyz);
+end
+
+if (handles(3) == 0)
+    h3 = slice3i(vol, T, 3, round(size(vol, 3) / 2), handles(3), keepxyz);
+else
+    h3 = slice3i(vol, T, 3, round(min(min(get(handles(3), 'zdata')))), handles(3), keepxyz);
+end
+
+if (nargout >= 1)
+    h = [h1, h2, h3];
+end
+
+set([h1, h2, h3], 'CDataMapping', 'scaled', varargin{:});
+
+% colormap(jet(64));
+
+view(3);
+if (all(handles == 0))
+    set(camlight, 'Visible', 'on');
+end
+
+axis equal;
---
+++ mcx/utils/mcxplotphotons.m
@@ -0,0 +1,54 @@
+function varargout = mcxplotphotons(traj, varargin)
+%
+%    mcxplotphotons(traj)
+%       or
+%    mcxplotphotons(traj, 'color','r','marker','o')
+%    [sorted, linehandle]=mcxplotphotons(traj)
+%
+%    Plot photon trajectories from MCXLAB's output
+%
+%    author: Qianqian Fang (q.fang <at> neu.edu)
+%
+%    input:
+%        traj: the 5th output of mcxlab, storing the photon trajectory info
+%           traj.id: the photon index being recorded
+%           traj.pos: the 3D position of the photon; for each photon, the
+%                  positions are stored in serial order
+%           traj.data: the combined output, in the form of
+%                [id,pos,weight,reserved]'
+%
+%    output:
+%        sorted: a structure to store the sorted trajectory info
+%        sorted.id: the sorted vector of photon id, staring from 0
+%        sorted.pos: the sorted position vector of each photon, only
+%                 recording the scattering sites.
+%
+%    this file is part of Monte Carlo eXtreme (MCX)
+%    License: GPLv3, see http://mcx.sf.net for details
+%
+
+if (isstruct(traj) && ~isfield(traj, 'id') && isfield(traj, 'data'))
+    traj = struct('id', typecast(single(traj.data(1, :)'), 'uint32'), 'pos', traj.data(2:4, :)', 'weight', traj.data(5, :)', 'data', traj.data);
+end
+if (~isstruct(traj) && size(traj, 2) == 6)
+    traj = struct('id', typecast(single(traj(:, 1)), 'uint32'), 'pos', traj(:, 2:4), 'weight', traj(:, 5));
+end
+
+[newid, idx] = sort(traj.id);
+lineend = (diff(newid) > 0);
+newidx = cumsum([0; lineend] + 1);
+newpos = nan(length(idx) + length(lineend), 4);
+newpos(newidx, 1:3) = traj.pos(idx, :);
+newpos(newidx, 4) = traj.data(5, :)';
+
+if (~(length(varargin) == 1 && strcmp(varargin{1}, 'noplot')))
+    edgealpha = 1 - (1 - (exist('OCTAVE_VERSION', 'builtin') ~= 0)) * 0.75;  % octave 6 returns empty if edgealpha<1
+    hg = patch(newpos(:, 1), newpos(:, 2), newpos(:, 3), newpos(:, 4), 'edgecolor', 'interp', 'edgealpha', edgealpha, varargin{:});
+    view(3);
+    axis equal;
+else
+    hg = [];
+end
+
+output = {struct('id', newid, 'pos', traj.pos(idx, :)), hg};
+[varargout{1:nargout}] = output{1:nargout};
